40f8a423b867ae3f66bd6efd329f8fa3fae60d5f
libjkjdxtj <2601454425@qq.com>
2018-09-15 11:11:56 +0800

    get studentnumber

---
+++ app/Http/Model/GetNumber.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GetNumber extends Model
+{
+    protected $table='tiyu';//表
+    protected $primaryKey='id';//主键
+    public $timestamps=false;//时间戳
+}
---
+++ resources/views/search.blade.php
@@ -0,0 +1,17 @@
+
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <title>体育成绩</title>
+</head>
+<body>
+
+{{--echo $echostr['id'];--}}
+{{--echo $echostr['student_name'];--}}
+{{--echo $echostr['student_score'];--}}
+{{$echostr['student_number']}}
+
+</body>
