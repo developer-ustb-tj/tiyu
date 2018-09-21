84b940191da6d133c817e01d94b8c4c396d88501
pdeantihuman <pdeantihuman@gmail.com>
2018-09-21 16:17:34 +0800

    use di wechat instance

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -5,20 +5,7 @@ use Log;
 
 class WeixinController extends Controller {
     public function verify() {
-        $config = [
-            'app_id' => 'wx8e19b6be727a39f2',
-            'secret' => 'd76ef29ba420ffe1529a737755e09c15',
-            'token' => 'makefun',
-            'response_type' => 'array',
-        
-            'log' => [
-                'level' => 'debug',
-                'file' => __DIR__.'/wechat.log',
-            ],
-        ];
-        
-        $app = Factory::officialAccount($config);
-        
+        $app = app('wechat.official_account');
         $response = $app->server->serve();
         
         return $response;
