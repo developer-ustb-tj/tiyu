ee7581e100d618344088cdd9dae1a0c1251459e6
pdeantihuman <pdeantihuman@gmail.com>
2018-09-23 09:37:07 +0800

    write a script to generate qr code

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,25 @@ Route::get('/api/log', 'Admin\LogController@list');
 Route::get('nginx',function(){
     return view('nginx/list');
 });
+
+Route::get('/qr', function(){
+    $data = 'https://www.youtube.com/watch?v=DLzxrzFCyOs&t=43s';
+    for($i=8;$i<=10;$i++){
+        for($j=1;$j<=4;$j++){
+            for($k=1;$k<=10;$k++){
+                $room=100*$j+$k;
+                $filename=$i.$room.'.png';
+                $path_prefix=$i.'/';
+                $prefix='https://report.sspider.cn/issues/create?';
+                is_writable(storage_path($i))?'':mkdir(storage_path($i));
+                $uri='alley='.$i.'&room='.$room;
+                $options = new \chillerlan\QRCode\QROptions([
+                    'cachefile'=> storage_path($path_prefix.$filename),
+                ]);
+                $qrcode = new \chillerlan\QRCode\QRCode($options);
+                $qrcode->render($prefix.$uri);
+            }
+        }
+    }
+    return 'hello world';
+});
