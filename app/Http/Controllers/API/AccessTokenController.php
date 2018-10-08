be2b82d3e52d27d212bbe49de1b3287e1afa97d8
pdeantihuman <pdeantihuman@gmail.com>
2018-10-08 23:55:35 +0800

    Update app/Http/Controllers/API/AccessTokenController.php

--- app/Http/Controllers/API/AccessTokenController.php
+++ app/Http/Controllers/API/AccessTokenController.php
@@ -7,8 +7,9 @@ use App\Http\Controllers\Controller;
 
 class AccessTokenController extends Controller
 {
-    public function index(){
-        $app=app('wechat.official_account');
+    public function index(Request $request){
+        $name = $request->name;
+        $app=\EasyWeChat::officialAccount($name);
         $access_token = $app->access_token;
         return $access_token->getToken()['access_token'];
     }
