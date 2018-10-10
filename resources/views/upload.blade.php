039a8aa92fd1a1ded1987f089e92aef79e2a2c30
libjkjdxtj <2601454425@qq.com>
2018-10-10 20:32:43 +0800

    Update resources/views/upload.blade.php

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -10,14 +10,11 @@
 
 </head>
     <style type="text/css">
-        body{
-            background: #f1e5e6;
-        }
         a{
             color: #d3394c;/*正常显示的*/
         }
         a:visited {
-            color:#00A600;
+            color:#d3394c;
             text-decoration:none;
         }
         a:hover {
@@ -27,28 +24,46 @@
     </style>
 </head>
 <body>
-<div class="links"  style="float: left; margin-top: 10%;margin-left:35%; width: 400px; height: 300px; background: rgba(225,0,0,0.1)" >
 
-    <div class="add_box" style="margin-left: 20px; margin-top: 28px;">
-        <span  onclick="downloadExcel()">上传之前务必点此<a href="/download">下载示例文档</a></span>
-    </div>
-    <br>
-    <form  method="POST" action="/a" enctype="multipart/form-data">
-        {{ csrf_field() }}
-        <label for="table" style="margin-left: 20px;">请选择excle文件</label>
-        <br><br>
-        <center>
-        <div class="box">
-            <input hidden="hidden" required type="file" name="table" id="file-4" class="inputfile inputfile-3" data-multiple-caption="{count} files selected" multiple />
-            <label for="file-4"><span>Choose a file&hellip;</span></label>
-        </div>
-        <br><br>
-        <input hidden="hidden" type="submit" name="file-1[]" id="file-1" class="inputfile inputfile-1" data-multiple-caption="{count} files selected" multiple />
-        <label for="file-1"><span>提交</span></label>
-        </center>
-    </form>
-
-</div>
 <script src="{{asset('js/custom-file-input.js')}}"></script>
+@extends('nginx.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">{{ __('上传文件') }}</div>
+
+                    <div class="card-body">
+                        <div class="links"  style="float: left; margin-top: 1%;margin-left:35%;" >
+
+                            <div class="add_box" style="margin-left: 20px; margin-top: 28px;">
+                                <span  onclick="downloadExcel()">上传之前务必点此<a href="/download">下载示例文档</a></span>
+                            </div>
+                            <br>
+                            <form  method="POST" action="/a" enctype="multipart/form-data">
+                                {{ csrf_field() }}
+                                <label for="table" style="margin-left: 20px;">请选择excle文件</label>
+                                <br><br>
+                                <center>
+                                    <div class="box">
+                                        <input hidden="hidden" required type="file" name="table" id="file-4" class="inputfile inputfile-3" data-multiple-caption="{count} files selected" multiple />
+                                        <label for="file-4"><span>Choose a file&hellip;</span></label>
+                                    </div>
+                                    <br><br>
+                                    <input hidden="hidden" type="submit" name="file-1[]" id="file-1" class="inputfile inputfile-1" data-multiple-caption="{count} files selected" multiple />
+                                    <label for="file-1"><span>提交</span></label>
+                                </center>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
 </body>
 </html>
